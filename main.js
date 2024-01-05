28e584834a035f4e8034eca52cfb4df69dbb6ae8
Aymen Saighi <aymensaighi95@gmail.com>
2024-01-05 23:55:46 +0100

    données popup

--- main.js
+++ main.js
@@ -40,50 +40,118 @@ fetch('lacs.json') // Remplacer par le chemin réel du fichier GeoJSON
             }
         });
       
-       // Modifier le curseur lorsqu'il passe sur un marqueur
-       map.on('mouseenter', 'markers', function(e) {
-        map.getCanvas().style.cursor = 'pointer';
-        // Assurez-vous que les propriétés sont présentes
-        if (!e.features.length) {
-            return;
-        }
+        map.on('mouseenter', 'markers', function(e) {
+            map.getCanvas().style.cursor = 'pointer';
+            if (!e.features.length) {
+                return;
+            }
+            
+            var coordinates = e.features[0].geometry.coordinates.slice();
+            var lacName = e.features[0].properties.name;
+            var description = e.features[0].properties.description;
+            var imageUrl = e.features[0].properties.imageUrl;
+            
+            var popupContent = `
+                <div class="popup-content">
+                    <h3>${lacName}</h3>
+                    <img src="${imageUrl}" alt="${lacName}" style="max-width:100%;"/>
+                    <p>${description}</p>
+                    <label for="rating">Notez votre experience:</label>
+                    <input type="number" id="rating" name="rating" min="1" max="5">
+                    <button onclick="submitRating()">Soumettre</button>
+                </div>
 
-        var coordinates = e.features[0].geometry.coordinates.slice();
-        var lacName = e.features[0].properties.name; // Assurez-vous que vos données GeoJSON ont une propriété 'description'
-        var description = e.features[0].properties.description; // Propriété 'description'
-        var imageUrl = e.features[0].properties.imageUrl; // Propriété 'imageUrl' pour l'image du lac
-       
+                <style> 
 
-        var popupContent = `
-            <div class="popup-content">
-                <h3>${lacName}</h3>
-                <img src= ${imageUrl} alt="${lacName}" style="max-width:100%;"/>
-                <p>${description}</p>
-                <label for="rating">Notez votre experience:</label>
-                <input type="number" id="rating" name="rating" min="1" max="5">
-            </div>
-        `;
+                .popup-content {
+                    border-radius: 10px; /* Arrondir les coins */
+                    background-color: rgba(255, 255, 255, 0.9); /* Fond légèrement transparent */
+                    backdrop-filter: blur(10px); /* Effet de flou derrière le popup */
+                   
+                    padding: 15px; /* Espace intérieur */
+                  }
+                  
+                  .popup-content h3 {
+                    text-align: center; /* Centre le texte */
+                    margin: 0; /* Enlève les marges par défaut pour un alignement précis */
+                    padding: 10px; /* Ajoute un peu de padding autour du texte pour l'esthétique */
+                    font-size: 1.5em; /* Ajuste la taille de la police si nécessaire */
+                    color: #333; /* Couleur du texte, à ajuster selon votre design */
+                  }
+                  /* Style pour l'image à l'intérieur du popup pour éviter le débordement */
+                  .popup-content img {
+                    max-width: 100%;
+                    height: auto;
+                    border-radius: 5px; /* Coins arrondis pour l'image */
+                    margin-bottom: 10px; /* Espace en dessous de l'image */
+                  }
+                  
+                  /* Style pour l'entrée de notation */
+                  .popup-content input[type="number"] {
+                    width: 100%; /* Prend toute la largeur disponible */
+                    padding: 5px; /* Espace intérieur */
+                    margin-top: 10px; /* Espace au-dessus de l'entrée */
+                    border: 1px solid #ccc; /* Bordure subtile */
+                    border-radius: 5px; /* Coins arrondis pour l'entrée */
+                  }
+                  
+                  /* Style pour le bouton de soumission de la note */
+                  .popup-content input[type="submit"] {
+                    background-color: #007bff; /* Couleur de fond */
+                    color: white; /* Couleur du texte */
+                    border: none; /* Pas de bordure */
+                    padding: 10px 20px; /* Espace intérieur */
+                    text-transform: uppercase; /* Texte en majuscules */
+                    margin-top: 10px; /* Espace au-dessus du bouton */
+                    cursor: pointer; /* Curseur en forme de main */
+                    border-radius: 5px; /* Coins arrondis pour le bouton */
+                    transition: background-color 0.3s ease; /* Transition pour le survol */
+                  }
+                  
+                  /* Effet de survol pour le bouton */
+                  .popup-content input[type="submit"]:hover {
+                    background-color: #0056b3; /* Couleur de fond plus foncée lors du survol */
+                  }
 
-        // Assurer que les popups s'affichent au-dessus des marqueurs
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-        }
-
-        
-           
-        new mapboxgl.Popup()
-        .setLngLat(coordinates)
-        .setHTML(popupContent)
-        .addTo(map); 
-
-        map.on('mouseleave', 'markers', function() {
-            map.getCanvas().style.cursor = '';
-            popup.remove();
+                  .mapboxgl-popup-close-button {
+                    font-size: 64px; /* Taille plus grande pour la croix */
+                    color: #007bff; /* Couleur de la croix */
+                    text-shadow: 0 1px 1px rgba(0, 0, 0, 0.7); /* Ombre pour améliorer la visibilité */
+                    opacity: 0.8; /* Légèrement transparente pour s'intégrer avec le design */
+                    margin-right : 5%;
+                    margin-top: 5%;
+                  }
+                  
+                  .mapboxgl-popup-close-button:hover {
+                    color: #0056b3; /* Couleur au survol */
+                    opacity: 1; /* Opacité totale au survol */
+                    cursor: pointer; /* Curseur en forme de main pour indiquer un élément cliquable */
+                  }
+                  
+                 </style> 
+            `;
+            
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+            
+            var popup = new mapboxgl.Popup()
+                .setLngLat(coordinates)
+                .setHTML(popupContent)
+                .addTo(map);
+            
+            map.on('mouseleave', 'markers', function() {
+                map.getCanvas().style.cursor = '';
+                //popup.remove(); // This will remove the popup when the mouse leaves the marker
+            });
         });
-
-
-
-    })
+        
+        function submitRating() {
+            var rating = document.getElementById('rating').value;
+            // Code here to handle the rating submission
+            console.log("Rating submitted: " + rating);
+        }
+        
 })
     .catch(error => {
         console.error('Erreur lors du chargement du GeoJSON:', error);
