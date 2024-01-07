84b1d0ea5d3e87d34762081e6f454c60c9225752
Suupon <aymensaighi95@gmail.com>
2024-01-07 22:38:18 +0100

    profil

--- profil.php
+++ profil.php
@@ -4,19 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="profil.css">
-   
+    <script>
+        function demanderDeconnexion() {
+            var confirmation = confirm("Voulez-vous vraiment vous déconnecter ?");
+            if (confirmation) {
+                window.location.href = "logout.php";
+            }
+        }
+    </script>
 </head>
 <body> 
+
     <div class="banner">
         <img src="images/bannière.png" alt="Bannière">
     
         <div id="profil"> 
-            <a href="accueil.html"><u> Retour Page d'Accueil >> </u></a> 
+            <a href="accueil.html"><u> Retour Page d'Accueil >> </u></a>
+            <?php
+            session_start();
+            if (isset($_SESSION['email'])) {
+                echo '<div id="deconnexion">';
+                echo '<a href="logout.php">Se Déconnecter</a>';
+                echo '</div>';
+            }
+            ?>
         </div>
     </div>
+
     <div id="information"> 
     <?php
-        session_start();
         if (isset($_SESSION['email'])) {
             echo '<h2> BONJOUR ' .  $_SESSION['prenom'] . ' ! </h2><br><br>';
             echo '<p>Nom : ' . $_SESSION['nom'] . '</p><br>';
@@ -27,8 +43,7 @@
             echo '<p> Connectez-vous pour afficher le profil.</p>';
         }
     ?>
-
     </div>
+
 </body>
 </html>
-
