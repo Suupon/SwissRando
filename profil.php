4d92fc1bd92f11bee3c3bb0228eef11a9b2fc0f5
Suupon <aymensaighi95@gmail.com>
2024-01-08 23:34:51 +0100

    bouton se déco ok

--- profil.php
+++ profil.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="profil.css">
-
     <script>
         function demanderDeconnexion() {
             var confirmation = confirm("Voulez-vous vraiment vous déconnecter ?");
@@ -13,40 +12,38 @@
             }
         }
     </script>
+
 </head>
 <body> 
 
     <div class="banner">
         <img src="images/bannière.png" alt="Bannière">
-    </div>
-
-    <div id="information"> 
+    
+        <div id="profil"> 
+            <a href="accueil.html"><u> Retour Page d'Accueil >> </u></a>
+        </div>
         <?php
-            session_start();
-            if (isset($_SESSION['email'])) {
-                echo '<h2> BONJOUR ' .  $_SESSION['prenom'] . ' ! </h2><br><br>';
-                echo '<p>Nom : ' . $_SESSION['nom'] . '</p><br>';
-                echo '<p>Prénom : ' . $_SESSION['prenom'] . '</p><br>';
-                echo '<p>Email : ' . $_SESSION['email'] . '</p><br>';
-                // Vous pouvez ajouter d'autres informations du profil si nécessaire
-            } else {
-                echo '<p> Connectez-vous pour afficher le profil.</p>';
-            }
-        ?>
-    </div>
-     
-    <div class="deco">
-        <?php
-       
+        session_start();
         if (isset($_SESSION['email'])) {
-            echo '<div id="profil">';
-            echo '<a href="accueil.html"><u> Retour Page d\'Accueil >> </u></a>';
-            echo '</div>';
-            echo '<div id="deco">';
-            echo '<a href="javascript:void(0);" onclick="demanderDeconnexion()">Se Déconnecter</a>';
+            echo '<div id="deconnexion">';
+            echo '<a href="#" onclick="demanderDeconnexion()">Se Déconnecter</a>';
             echo '</div>';
         }
         ?>
+    </div>
+
+    <div id="information"> 
+    <?php
+        if (isset($_SESSION['email'])) {
+            echo '<h2> BONJOUR ' .  $_SESSION['prenom'] . ' ! </h2><br><br>';
+            echo '<p>Nom : ' . $_SESSION['nom'] . '</p><br>';
+            echo '<p>Prénom : ' . $_SESSION['prenom'] . '</p><br>';
+            echo '<p>Email : ' . $_SESSION['email'] . '</p><br>';
+            // Vous pouvez ajouter d'autres informations du profil si nécessaire
+        } else {
+            echo '<p> Connectez-vous pour afficher le profil.</p>';
+        }
+    ?>
     </div>
 
 </body>
