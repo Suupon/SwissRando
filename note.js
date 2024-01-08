4e2b2af4530a997a91e19be65121df57de17d04e
Aymen Saighi <aymensaighi95@gmail.com>
2024-01-08 22:19:33 +0100

    enregistrement des itinéraires en local

--- note.js
+++ note.js
@@ -10,60 +10,53 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 var submitButton = document.querySelector("#submit-btn");
-submitButton.addEventListener('click', function() {
-    console.log('Le bouton a été cliqué');
-});
+    
+            submitButton.addEventListener('click', function() {
 
-            submitButton.addEventListener('click', function submitratings() {
-
-                //Vérifie si toutes les case sont remplies
-                var ratingInputs = document.querySelectorAll('#tabs-2 table input[type="number"]');
-                var allFilled = true;
-
-                ratingInputs.forEach(function(input) {
-                    if (!input.value) {
-                        allFilled = false;
-                    }
-                });
-
-                if (!allFilled) {
-                    alert("Veuillez remplir toutes les notes avant de soumettre.");
-                    return; // Arrête l'exécution de la fonction
-                }
-                console.log("jsuis ici")
+                  //Vérifie si toutes les case sont remplies
+                  var ratingInputs = document.querySelectorAll('#tabs-2 table input[type="number"]');
+                  var allFilled = true;
+  
+                  ratingInputs.forEach(function(input) {
+                      if (!input.value) {
+                          allFilled = false;
+                      }
+                  });
+  
+                  if (!allFilled) {
+                      alert("Veuillez remplir toutes les notes avant de soumettre.");
+                      return; // Arrête l'exécution de la fonction
+                  }
                 var ratings = [];
                 var rows = document.querySelectorAll('#tabs-2 table tr');
                 rows.forEach((row, index) => {
-                    if (index > 0) { // Ignorer l'en-tête du tableau
+                    if (index > 0) {
                         var lakeName = row.cells[1].innerText;
-                        var rating = row.cells[2].querySelector('input').value;
+                        var rating = parseInt(row.cells[2].querySelector('input').value);
                         ratings.push({ lakeName: lakeName, note: rating });
                     }
                 });
-                var jsonRatings = JSON.stringify(ratings);
-                console.log(jsonRatings).
-               
+            
                 fetch('Notes.php', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
                     },
-                    body: jsonRatings // les données collectées
+                    body: JSON.stringify(ratings)
                 })
                 .then(response => response.json())
                 .then(data => {
-                    // Traiter la réponse du serveur ici
                     console.log(data);
                 })
                 .catch((error) => {
                     console.error('Error:', error);
                 });
-            
-            
-                
+
+
                 ratingInputs.forEach(function(input) {
                     var textSpan = document.createElement('span');
                     textSpan.textContent = input.value;
                     input.parentNode.replaceChild(textSpan, input);
                 });
             });
+            
