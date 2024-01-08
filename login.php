d05980d72fad867a4a20dffa7067e3e43a4b2876
Suupon <aymensaighi95@gmail.com>
2024-01-08 22:22:24 +0100

    ok

--- login.php
+++ login.php
@@ -4,32 +4,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="formu.css">
-    <title>Connexion</title>
+    <title>inscription</title>
 </head>
-<body>
+
+<body> 
+
     <div class="titre"> 
         <h1>
             SWIS'sRANDO
         </h1>
     </div>
-    <div class="container">
-        <form action="login_process.php" method="post">
-            <div>
-                <label>Email</label>
-                <input type="text" name="email" placeholder="Entrer votre email">
+
+    <div class = "container"> 
+        <form action="connect.php" method="post">   
+
+
+            <div class="civ">
+                <label>Civilité</label>
+                <input type="radio" name= "civilite"><label>Mr</label>
+                <input type="radio" name= "civilite"><label>Mme</label>
+            </div>
+           
+            <div class="nom">
+                <label>Nom</label>
+                <input type="text" name= "name" placeholder = "Entrer votre nom">
             </div>
 
-            <div>
-                <label>Mot de Passe</label>
-                <input type="password" name="mdp" placeholder="Entrer votre Mot de passe">
+            <div class="prenom">
+                <label>Prénom</label>
+                <input type="text" name= "prenom" placeholder = "Entrer votre prénom">
+            </div>
+
+            <div class="mail">
+                <label>Email</label>
+                <input type="text" name= "email" placeholder = "Entrer votre email">
+            </div>
+
+            <div class="mdp">
+                <label>Mot de Passe </label>
+                <input type="password" name= "mdp" placeholder = "Entrer votre Mot de passe">
             </div>
 
             <div class="btn">
-                <button type="submit">Se Connecter</button>
+                <button type= "submit"> Envoyer </button>
             </div>
+    
         </form>
-
-        <p>Vous n'avez pas de compte ? <a href="index.php">S'inscrire</a></p>
     </div>
 </body>
 </html>
+
